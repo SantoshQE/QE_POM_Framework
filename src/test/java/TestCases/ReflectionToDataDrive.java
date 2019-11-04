5569d2ca8ae1174bd1084451254db109b7dedfcf
SantoshGit <santosh.pandhare@gmail.com>
2019-11-04 23:20:45 +0530

    changes to reflection code

--- src/test/java/TestCases/ReflectionToDataDrive.java
+++ src/test/java/TestCases/ReflectionToDataDrive.java
@@ -2,18 +2,12 @@ package TestCases;
 
 import Config.TestBase;
 import Utils.ExcelReader;
-import java.lang.annotation.Annotation;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.HashMap;
-import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
-import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -41,15 +35,27 @@ public class ReflectionToDataDrive extends TestBase
     HashMap<Integer, String> ReflectMap = new HashMap<>();
     public static int rowCount;
     static StringBuilder sb;
-  //  public static WebDriver driver = TestBase.getDriver();
-    public static WebDriver driver;
-    public ReflectionToDataDrive(WebDriver driver)
-    {
+    //  public static WebDriver driver = TestBase.getDriver();
+    // public static WebDriver driver=TestBase.getDriver();
+    public static String className;
+    public static Object objClass;
+
+    public ReflectionToDataDrive(String className) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         String DataPath =  System.getProperty("user.dir")+"/src/main/resources/TestData/TestData.xlsx";
         ExL = new ExcelReader(DataPath);
         System.out.println(ExL);
-        this.driver = driver;
+        //   this.driver = driver;
+        readClass(className);
+        //objClass = TestCaseClassName.getConstructor().newInstance();
+        System.out.println(getObject());
+
     }
+    public Object getObject() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException
+    {
+        objClass = TestCaseClassName.getConstructor().newInstance();
+        return objClass;
+    }
+
     public int getRowCountReflectDD(String sheetName)
     {
         rowCount = ExL.getRowCount(sheetName);
@@ -66,73 +72,90 @@ public class ReflectionToDataDrive extends TestBase
         }
         return TestCaseClassName;
     }
-    public void countAnnotations(String className) throws Exception
+    public void dataDriveTest(String className) throws Exception
     {
-        TestCaseClassInstance = readClass(className);
-        Method[] methods = TestCaseClassInstance.getDeclaredMethods();
-        for (int i = 0; i < methods.length; i++)
-        {
-            System.out.println("public method: " + methods[i]);
-            if(methods[i].getAnnotation(AfterTest.class) != null)    // @AfterTest Annotated method
-            {
-                System.out.println(methods[i].getAnnotation(AfterTest.class).description());
-            }
-            if(methods[i].getAnnotation(BeforeTest.class) != null)   // @BeforeTest Annotated method
-            {
-                System.out.println(methods[i].getAnnotation(BeforeTest.class).description());
+        int totalDataRows = getRowCountReflectDD("TestData");
 
-            }
-            if(methods[i].getAnnotation(Test.class) != null) // @Test Annotated method
-            {
-                System.out.println(methods[i].getAnnotation(Test.class).priority());
-                int runPriority = methods[i].getAnnotation(Test.class).priority();
-                ReflectMap.put(runPriority,methods[i].getName());
-            }
-        }
-        //Return number of rows from testdata sheet matching the testcase class name and invoke the methods
-        rowCount = ExL.getRowCount("TestData");
-        int dataIter = 0;
-        for( dataIter =1;dataIter<= rowCount;dataIter++)
-        {
-            for (Integer key : ReflectMap.keySet())
+
+                TestCaseClassInstance = readClass(className);
+                Method[] methods = TestCaseClassInstance.getDeclaredMethods();
+                //load all the methods with @Test annotation in a HashMap
+                for (int testIter = 0; testIter< methods.length; testIter++)
+                {
+                    if (methods[testIter].getAnnotation(Test.class) != null) // @Test Annotated method
+                    {
+                        System.out.println(methods[testIter].getAnnotation(Test.class).priority());
+                        int runPriority = methods[testIter].getAnnotation(Test.class).priority();
+                        ReflectMap.put(runPriority, methods[testIter].getName());
+                    }
+                }
+                //Find method with BeforeTest Annotation first and execute that method first
+                    /*        for (int beforeTestIter = 0; beforeTestIter< methods.length; beforeTestIter++) {
+                                System.out.println("public method: " + methods[beforeTestIter]);
+                                if (methods[beforeTestIter].getAnnotation(BeforeTest.class) != null)    // @BeforeTest Annotated method
+                                {
+                                    System.out.println(methods[beforeTestIter].getAnnotation(BeforeTest.class).description());
+                                    Method beforeTestMName = TestCaseClassName.getDeclaredMethod(methods[beforeTestIter].getName());
+                                    beforeTestMName.invoke(objClass);
+                                }
+                            }*/
+                //Iterate throught he hashmap and run the @Test methods in sequence as per priority i.e. @Test(priority=1,2,...n)
+                for (Integer key : ReflectMap.keySet())
                 {
                     System.out.println("Method Priority is : "+key+"   for method named : "+ReflectMap.get(key));
-                    Object objClass = TestCaseClassName.getConstructor().newInstance();
                     String methodName = ReflectMap.get(key);
                     Method TestMethodName = TestCaseClassName.getDeclaredMethod(methodName);
                     if(TestMethodName.isAnnotationPresent(Test.class))
                     {
-                        TestMethodName.invoke(objClass, null);
+                        TestMethodName.invoke(objClass);
                     }
                 }
-        }
+                //Find method with AfterTest Annotation at the end and execute that method
+                for (int afterTestIter = 0; afterTestIter< methods.length; afterTestIter++)
+                {
+                    System.out.println("public method: " + methods[afterTestIter]);
+                    if (methods[afterTestIter].getAnnotation(AfterTest.class) != null)    // @AfterTest Annotated method
+                    {
+                        System.out.println(methods[afterTestIter].getAnnotation(AfterTest.class).description());
+                        Method afterTestMName = TestCaseClassName.getDeclaredMethod(methods[afterTestIter].getName());
+                        afterTestMName.invoke(objClass);
+                        break;
+                    }
+                }
+                //Return number of rows from testdata sheet matching the testcase class name and invoke the methods
+                    /*        rowCount = ExL.getRowCount("TestData");
+                            int dataIter = 0;
+                            for( dataIter =1;dataIter<= rowCount;dataIter++)
+                            {*/
 
-/*        for (Integer key : ReflectMap.keySet())
-        {
-            System.out.println("Method Priority is : "+key+"   for method named : "+ReflectMap.get(key));
-            Object objClass = TestCaseClassName.getConstructor().newInstance();
-            String methodName = ReflectMap.get(key);
-            Method TestMethodName = TestCaseClassName.getDeclaredMethod(methodName);
-            if(TestMethodName.isAnnotationPresent(Test.class))
-            {
-                TestMethodName.invoke(objClass, null);
+                /*        }*/
+
+                    /*        for (Integer key : ReflectMap.keySet())
+                            {
+                                System.out.println("Method Priority is : "+key+"   for method named : "+ReflectMap.get(key));
+                                Object objClass = TestCaseClassName.getConstructor().newInstance();
+                                String methodName = ReflectMap.get(key);
+                                Method TestMethodName = TestCaseClassName.getDeclaredMethod(methodName);
+                                if(TestMethodName.isAnnotationPresent(Test.class))
+                                {
+                                    TestMethodName.invoke(objClass, null);
+                                }
+                            }*/
+                //  driver.close();
+
+
             }
-        }*/
-     //  driver.close();
+                    /*    public static void runAllAnnotatedWith(Class<? extends Annotation> annotation) throws Exception
+                        {
+                            Reflections reflections = new Reflections(new ConfigurationBuilder()
+                                    .setUrls(ClasspathHelper.forJavaClassPath())
+                                    .setScanners(new MethodAnnotationsScanner()));
+                            Set<Method> methods = reflections.getMethodsAnnotatedWith(annotation);
 
-
-    }
-/*    public static void runAllAnnotatedWith(Class<? extends Annotation> annotation) throws Exception
-    {
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setUrls(ClasspathHelper.forJavaClassPath())
-                .setScanners(new MethodAnnotationsScanner()));
-        Set<Method> methods = reflections.getMethodsAnnotatedWith(annotation);
-
-        for (Method m : methods)
-        {
-            m.invoke(null);
-        }
-    }*/
+                            for (Method m : methods)
+                            {
+                                m.invoke(null);
+                            }
+                        }*/
 }
 
