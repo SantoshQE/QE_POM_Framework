2fd7a3a5c8fa972ee192119cc8434c68a185c033
SantoshGit <santosh.pandhare@gmail.com>
2019-11-10 22:19:06 +0530

    changes made to code logic

--- src/test/java/TestCases/DatadrivenTest.java
+++ src/test/java/TestCases/DatadrivenTest.java
@@ -2,12 +2,14 @@ package TestCases;
 
 import Config.TestBase;
 import ExtentReportListener.ExtentManager;
+import Pages.twitLoginPage;
 import org.testng.annotations.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import com.aventstack.extentreports.ExtentTest;
 import Listeners.CustomListeners;
 import TestCases.ReflectionToDataDrive;
+import static TestCases.TC3_twitLoginChrome.twtLoginPg;
 
 @Listeners({CustomListeners.class})
 public class DatadrivenTest extends TestBase
@@ -15,13 +17,9 @@ public class DatadrivenTest extends TestBase
     public static ReflectionToDataDrive ReflectDD;
     public static ExtentTest test;
 
-/*    public DatadrivenTest() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException
+    @BeforeMethod(description = "This is used to pass testname,description to extent report")
+    public void setup(Method method)
     {
-*//*        ReflectDD = new ReflectionToDataDrive("DatadrivenTest");
-        ReflectDD.readClass("DatadrivenTest");*//*
-    }*/
-    @BeforeMethod
-    public void setup(Method method) {
         String testMethodName = method.getName();
         String descriptiveTestName = method.getAnnotation(Test.class).testName();
         test = ExtentManager.startTest(descriptiveTestName, ExtentManager.getLabel(method.getDeclaringClass().getName()));
@@ -30,26 +28,20 @@ public class DatadrivenTest extends TestBase
     public void startUp() throws Exception
     {
         ReflectDD = new ReflectionToDataDrive("DatadrivenTest");
-        //ReflectDD.readClass("DatadrivenTest");
-        System.out.println("StartUp ...");
-/*        //ReflectDD.readClass("TestCases.DatadrivenTest");
-      //  System.out.println( ReflectDD.readClass("DatadrivenTest")); */
         ReflectDD.dataDriveTest("DatadrivenTest");
     }
-    @Test(priority=1,testName ="TestStep1")
-    public void LaunchApplication()
+    @Test(priority = 1,testName ="TC3_twitLoginChrome--TestNGDescription :-- Log into twitter account")
+    public void twitterLogin_Chrome() throws Throwable
     {
-        System.out.println(this.getClass().getName());
-        System.out.println("Inside @BeforeTest --launchBrowser_Chrome ");
         TestBase.open_Browser("Chrome");
+        System.out.println("twitterLogin_Chrome");
+        ExtentManager.getTest().assignCategory("Regression");
     }
-    @Test(priority=2,testName ="TestStep1")
-    public void TestStep1()
-    {
-        System.out.println("Total rows in testdata is : "+ReflectDD.getRowCountReflectDD("TestData"));
-        System.out.println("TestStep..1");
+    @Test(priority = 2,testName ="TC3_twitLoginChrome--TestNGDescription :-- Navigate to twitter profile page" )
+    public void navigateToProfile_Chrome() throws Throwable {
+        System.out.println("navigateToProfile_Chrome");
+        ExtentManager.getTest().assignCategory("Regression");
     }
-
      @AfterTest(description="AfterTest")
     public void testDown()
     {
